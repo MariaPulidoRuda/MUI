3d346424ecd077a755b28c82227a21bcf8821ebb
Maria Pulido ruda <mariapulidoruda@MacBook-Air-de-Maria.local>
2022-10-30 20:41:04 +0100

    MUI

---
+++ app/src/App.jsx
@@ -0,0 +1,19 @@
+import QuiltedImageList from './components/Gallery'
+import ResponsiveAppBar from './components/Navbar'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+    <ResponsiveAppBar />
+    <QuiltedImageList />
+    
+  
+    </div>
+  )
+}
+
+export default App
